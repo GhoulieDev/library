feada80cfa1fbc94af61c0af5de583d7bf155511
Mark <markrodgers1@hotmail.com>
2021-07-26 18:13:11 +0100

    Add flex properties to allow for dynamic alignment of book items

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const bookContainer = document.getElementById('library')
+
 let myLibrary = [];
 
 function Book(title, author, pages, haveRead){
@@ -11,23 +13,28 @@ function addBookToLibrary(){
     myLibrary.push(harry);
     myLibrary.push(game);
     myLibrary.push(witcher);
-    myLibrary.push(shan);
+    
+    
 }
 
 function displayBooks(library){
     for(let book of library){
-        console.log(book.title)
+        
+        
     }
 
 }
 
+
+
 const harry = new Book('Harry Potter', 'JK Rowling', '250', false)
 const game = new Book('Game Of Thrones', 'George RR', '650', true)
 const witcher = new Book('Witcher', 'Androv Sapjowski', '500', true)
-const shan = new Book('Cirque Du Freak', 'Darren Shan', '305', true)
+
 
 addBookToLibrary()
 displayBooks(myLibrary)
+
 
 
 
